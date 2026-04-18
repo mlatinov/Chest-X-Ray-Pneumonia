a7c875bd3c9d741b1bfe32eeaacaedec7001d784
mlatinov <latinovmetodi@gmail.com>
2026-04-18 22:49:06 +0300

    reduce the complexity of the resnet model for short training and reduced overfiting

--- scripts/cnn_ResNet_style.py
+++ scripts/cnn_ResNet_style.py
@@ -6,25 +6,25 @@ class Resnet(tf.keras.Model) :
         super().__init__()
 
         # Initial Feature Extraction 
-        self.feature_extraction = modern_block(filters = 32, kernel_size = 3, activation = activation, pool_size = 2)
+        self.feature_extraction = modern_block(filters = 8, kernel_size = 3, activation = activation, pool_size = 2)
 
         # Learn Residuals 
-        self.resid_block_1 = residual_block(filters = 64, kernel_size = 3, activation = activation)
-        self.resid_block_2 = residual_block(filters = 64, kernel_size = 3, activation = activation)
+        self.resid_block_1 = residual_block(filters = 16, kernel_size = 3, activation = activation)
+        self.resid_block_2 = residual_block(filters = 16, kernel_size = 3, activation = activation)
 
         # Downsample 
         self.pool_1 = tf.keras.layers.MaxPool2D(pool_size = 2)
 
         # Learn Residuals 
-        self.resid_block_3 = residual_block(filters = 128, kernel_size = 3, activation = activation)
-        self.resid_block_4 = residual_block(filters = 128, kernel_size = 3, activation = activation)
+        self.resid_block_3 = residual_block(filters = 32, kernel_size = 3, activation = activation)
+        self.resid_block_4 = residual_block(filters = 32, kernel_size = 3, activation = activation)
 
         # Downsample 
         self.pool_2 = tf.keras.layers.MaxPool2D(pool_size = 2)
 
         # Head 
         self.global_pool = tf.keras.layers.GlobalAveragePooling2D()
-        self.drop        = tf.keras.layers.Dropout(rate = 0.4)
+        self.drop        = tf.keras.layers.Dropout(rate = 0.5)
         self.classifier  = tf.keras.layers.Dense(units = 1, activation = "sigmoid")
 
     def call(self, x , training = False) :
