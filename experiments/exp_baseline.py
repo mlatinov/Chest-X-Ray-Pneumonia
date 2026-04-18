6fc299611d0cef6b718051107f5e0fab9f7923fb
mlatinov <latinovmetodi@gmail.com>
2026-04-18 18:17:54 +0300

    Baseline model Experiment

---
+++ experiments/exp_baseline.py
@@ -0,0 +1,57 @@
+
+import mlflow
+import tensorflow as tf
+from scripts.cnn_baseline import Baseline
+from scripts.process_image import process_data
+from scripts.plot_history import plot_history
+
+def exp_baseline_cnn() :
+    # Log Everything 
+    mlflow.tensorflow.autolog()
+
+    # Start the experiment 
+    with mlflow.start_run(run_name = "Baseline CNN") :
+
+        # Process data 
+        model_data = process_data(
+            dir_train_path      = "sample_data_img/train/",
+            dir_validation_path = "sample_data_img/val/",
+            img_size            = (224, 224)
+        )
+        # Get the model 
+        baseline_model = Baseline(activation = "relu")
+
+        # Compile the model 
+        baseline_model.compile(
+            loss      = tf.keras.losses.BinaryCrossentropy(),
+            optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4),
+            metrics   = [
+                tf.keras.metrics.AUC(),
+                tf.keras.metrics.Recall(),
+                tf.keras.metrics.Precision()
+            ]
+        )
+        # Fit the model 
+        history =  baseline_model.fit(
+            x               = model_data["train_samples"],
+            validation_data = model_data["val_samples"],
+            epochs          = 20,
+            callbacks       = [
+                tf.keras.callbacks.EarlyStopping(
+                    monitor              = "val_loss",
+                    patience             = 3,
+                    restore_best_weights = True,
+                    verbose              = 1
+                ),
+                tf.keras.callbacks.ReduceLROnPlateau(
+                    monitor  = "val_loss",
+                    factor   = 0.5,
+                    patience = 2,
+                    min_lr   = 1e-6,
+                    verbose  = 1 
+                )
+            ]
+        )
+        # Ploting and logging the metrics 
+        fig = plot_history(history)
+        mlflow.log_figure(fig, "training_curves.png")
